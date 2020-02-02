c0c58a498f0f296085079213e41e65fd1680a928
Michal Stehlík <michal.stehlik@pslib.cz>
2020-02-02 20:56:44 +0100

    Add project files.

---
+++ FirstUWPApp/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FirstUWPApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        private string _text;
+        private double _size;
+
+        public string Text { get { return _text; } set { _text = value; NotifyPropertyChanged(); } }
+        public double Size { get { return _size; } set { _size = value; NotifyPropertyChanged(); } }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public MainPage()
+        {
+            Text = "Hello World";
+            Size = 24;
+            this.InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ FirstUWPApp/ViewModel/TextPropertiesViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstUWPApp.ViewModel
+{
+    class TextPropertiesViewModel : INotifyPropertyChanged
+    {
+        private string _text;
+        private double _size;
+
+        public TextPropertiesViewModel()
+        {
+            Text = "Ahoj světe";
+            Size = 32;
+        }
+
+        public string Text { get { return _text; } set { _text = value; NotifyPropertyChanged(); } }
+        public double Size { get { return _size; } set { _size = value; NotifyPropertyChanged(); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
